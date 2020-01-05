74dced6ef88b330a481f060005d33892d2608e3b
lxl <971239524@qq.com>
2020-01-05 13:25:30 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
+      <h3>
+        二零二零，开年大吉！
+      </h3>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
